85fb5246f3c799ade87e6baffba49aa6ffb9028b
beryl470 <khalaiberyl.gmail.com>
2025-11-18 11:41:27 +0300

    change if else statement

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,26 @@
 const temperature = 90;
 const timeOfDay = "morning";
-let soilMoisture = 30;
+ 
+//watering control
+if (temperature>=80){
+console.log("watering on")
+} else { 
+    console.log("watering off")
+}
+
+//gardening control
+if (timeOfDay==="evening" || timeOfDay==="night"){
+    console.log("lights on")
+} else {
+    console.log('lights off')
+}
+
+//adjustment
+let soilMoisture = 0; 
+
+while (soilMoisture < 40) {
+    console.log(soilMoisture);
+    soilMoisture += 5;
+}
+
+console.log(soilMoisture); 
